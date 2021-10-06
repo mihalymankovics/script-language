f048d154a0555bce61c0f82bf0ccaad0540aee63
mihalymankovics <91718573+mihalymankovics@users.noreply.github.com>
2021-10-07 01:07:52 +0200

    Add files via upload

---
+++ secret_logic.py
@@ -0,0 +1,17 @@
+def is_numeric(text):
+    return text.isnumeric()
+
+def ls_supported_operator(text):
+    return text in ['+', '-', '*', '/']
+
+def calculate(operand1, p_operator, operand2):
+    rv = 0
+    if p_operator == '+':
+        rv = operand1 + operand2
+    elif p_operator == '-':
+        rv = operand1 - operand2
+    elif p_operator == '*':
+        rv = operand1 * operand2
+    elif p_operator == '/':
+        rv = operand1 / operand2
+    return rv
